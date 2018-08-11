fab8024b78f1fb4d541084fa8d278c3c9f9bfd66
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-11 06:35:24 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,3 @@
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title text, category TEXT, funding_goal TEXT, start_date text, end_date text);
+CREATE TABLE users (id INTEGER PRIMARY KEY, name text, age integer);
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount integer, user_id integer, project_id TEXT);
